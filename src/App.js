1d667a256791e6366219c18d42ec04f9061a29df
shirantha <shirantha.fonseka97@gmail.com>
2025-05-10 21:01:51 -0400

    Add a sub component to app component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@ import "./styles.css";
 
 //vriables
 let subject = "JavaScript";
+function Header() {
+  return (
+    <header>
+      <p>This is a sub componemt of App componemt.</p>
+    </header>
+  );
+}
 
 export default function App() {
   return (
     <div className="App">
       <h1>Hello {subject.toUpperCase()}</h1>
+      <Header/>
     </div>
   );
 }
