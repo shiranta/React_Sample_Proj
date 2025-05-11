64fe94946e5d42893a1f1786863abf452039bdb8
shirantha <shirantha.fonseka97@gmail.com>
2025-05-10 21:22:27 -0400

    Add a a prop in a granular approch

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import "./styles.css";
 
 //vriables
 let subject = "JavaScript";
-function Header(props) {
+function Header({name,year}) {
+  //props are used to get data dynamically.
   return (
     <header>
-      <h2>This is {props.name}'s Kitchen.</h2>
+      
+      <h2>This is {name}'s Kitchen.</h2>
       <p>This is a sub componemt of App componemt.</p>
+      <p>CopyRight@: {year}</p>
     </header>
   );
 }
@@ -15,7 +18,7 @@ export default function App() {
   return (
     <div className="App" >
       <h1>Hello {subject.toUpperCase()}</h1>
-      <Header name="Anton"/>
+      <Header name="Anton" year={2025}/>
     </div>
   );
 }
