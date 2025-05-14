9e7422ca91665405d2549a8714405aa839fc0d1a
shirantha <shirantha.fonseka97@gmail.com>
2025-05-14 15:02:52 -0400

    useStatenested

--- src/App.js
+++ src/App.js
@@ -18,14 +18,16 @@ function Header({name,year}) {
 }
 const items=["Rice & curry","Kottu","Noodles","Pizza","Burger"];
 
-function Main({dishes}) {
-   const [status,setstatus] =useState("Kitchen Open");
-      console.log(status);
+function Main({dishes ,openstatus}) {
+  //dishes is the array of items that we are passing to the Main component.
+   
+
   //useState is a hook that allows us to use state in functional components.
   //useState returns an array with two elements: the current state and a function to update it.
   return (
     <main>
-      <h2>Resturent {status}</h2>
+      <h1>our beutiful resturent is{" "}{openstatus ?"open":"close"}</h1>
+      
       <ul>
         {dishes.map((dish=><li>{dish}</li>))}
       </ul>
@@ -34,7 +36,9 @@ function Main({dishes}) {
 }
 
 export default function App() {
- 
+ const [status,setstatus] =useState(true);   
+   console.log(status);
+  
   return (
     <>
     <div>
@@ -46,14 +50,21 @@ export default function App() {
       <div className="App" >
       <h1>Hello! Welocome to {subject.toUpperCase()}</h1>
 
-      <img src={chef} height={200} alt="chef" ></img>//add image
+      <img src={chef} height={200} alt="chef" ></img>
+      <h1>Resturent is currently {""}</h1>
+      {status ? "open" : "close"}
+      <button onClick={() => setstatus(!status)}>Resturent {""}
+        {status ? "close" : "open"}
+      </button>
+
       
+      <Main dishes={items} openstatus={status}/>
       
-      <Main dishes={items} />
       <Header name="Anton" year={2025}/>
     </div>
     </main>
     </>// By doing a fragmentation we can include divs and other elements
     // without wrapping them in a single parent element.
+    //we can pass a useState variable to a child component as a prop.
   );
 }
